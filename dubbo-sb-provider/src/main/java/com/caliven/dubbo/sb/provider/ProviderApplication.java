dd193fdb3296d07327a82e2d33b9c7e9a8e7f4d2
Caliven <liuzhihuil@163.com>
2018-09-09 22:14:32 +0800

    spring boot subbo

--- dubbo-sb-provider/src/main/java/com/caliven/dubbo/sb/provider/ProviderApplication.java
+++ dubbo-sb-provider/src/main/java/com/caliven/dubbo/sb/provider/ProviderApplication.java
@@ -1,9 +1,11 @@
 package com.caliven.dubbo.sb.provider;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@EnableDubbo
 @SpringBootApplication
 public class ProviderApplication {
 
--- dubbo-sb-provider/src/main/java/com/caliven/dubbo/sb/provider/service/DefaultDemoService.java
+++ dubbo-sb-provider/src/main/java/com/caliven/dubbo/sb/provider/service/DefaultDemoService.java
@@ -3,16 +3,21 @@ package com.caliven.dubbo.sb.provider.service;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.caliven.dubbo.api.DemoService;
 
-@Service/*(
-        version = "${provider.service.version}",
+@Service(
+        version = "${provider.service.version}"/*,
         application = "${dubbo.application.id}",
         protocol = "${dubbo.protocol.id}",
-        registry = "${dubbo.registry.id}"
-)*/
+        registry = "${dubbo.registry.id}"*/
+)
 public class DefaultDemoService implements DemoService {
 
     @Override
     public String sayHello(String name) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "Hello Dubbo, " + name + " (from Spring Boot)";
     }
 
